d2f5f6d6c585ec04ad0ee0a75e10c19e57a4e52a
novialia <novialia12@gmail.com>
2017-05-05 00:33:15 +0700

    sirimabasdat

--- js/homejs.js
+++ js/homejs.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 	 if(sessionStorage.getItem("username") === "admin") {
         $("#login-reg-nav").remove();
         var logout = "<li><a href= \"#\" id= \"logout-btn\">Log Out</a></li>";
-        var pageadmin = "<li class=\"dropdown\"><a href=\"#\">Laman <i class=\"fa fa-angle-down\"></i></a> <ul role=\"menu\" class=\"sub-menu\"><li> <a href= \"#\">Rekap Pendaftaran</a> </li><li><a href= \"#\">Daftar Pelamar</a></li></ul></li>";
-        var greetings = "<div class=\"col-md-4 col-md-offset-4\"><div class=\"alert alert-info fade in\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">×</button><p>Selamat datang, Admin!</p></div></div>";
+        var pageadmin = "<li class=\"dropdown\"><a href=\"#\">Laman Admin <i class=\"fa fa-angle-down\"></i></a> <ul role=\"menu\" class=\"sub-menu\"><li><a href= \"link-rekap-pendaftaran\">Rekap Pendaftaran</a></li><li><a href= \"link-daftar-pelamar\">Daftar Pelamar</a></li></ul></li>";
+        var greetings = "<div><div class=\"alert alert-info fade in\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">×</button><p>Selamat datang, Admin!</p></div></div>";
         $("#for-alert").append(greetings);
         $("#ul-nav-bar").append(pageadmin);
         $("#ul-nav-bar").append(logout);
@@ -11,8 +11,8 @@ $(document).ready(function() {
     } else if (sessionStorage.getItem("username") === "user") {
         $("#login-reg-nav").remove();
         var logout = "<li><a href= \"#\" id= \"logout-btn\">Log Out</a></li>";
-        var pageuser = "<li class=\"dropdown\"><a href=\"#\">Laman <i class=\"fa fa-angle-down\"></i></a> <ul role=\"menu\" class=\"sub-menu\"><li> <a href= \"#\">Buat Pendaftaran</a> </li><li> <a href= \"#\">Riwayat Pendaftaran</a></li><li> <a href= \"#\">Kartu Ujian</a> </li><li> <a href= \"#\">Hasil Seleksi</a> </li><li>";
-        var greetings = "<div class=\"col-md-4 col-md-offset-4\"><div class=\"alert alert-info fade in\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">×</button><p>Selamat datang, Grace Angelica!</p></div></div>";
+        var pageuser = "<li class=\"dropdown\"><a href=\"#\">Informasi SIRIMA <i class=\"fa fa-angle-down\"></i></a> <ul role=\"menu\" class=\"sub-menu\"><li> <a href= \"link-buat-pendaftaran\">Buat Pendaftaran</a> </li><li> <a href= \"riwayatdaftar.html\">Riwayat Pendaftaran</a></li><li> <a href= \"kartuujian.html\">Kartu Ujian</a> </li><li> <a href= \"link-hasil-seleksi\">Hasil Seleksi</a> </li><li>";
+        var greetings = "<div><div class=\"alert alert-info fade in\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">×</button><p>Selamat datang, Grace Angelica!</p></div></div>";
         $("#for-alert").append(greetings);
         $("#ul-nav-bar").append(pageuser);
         $("#ul-nav-bar").append(logout);
