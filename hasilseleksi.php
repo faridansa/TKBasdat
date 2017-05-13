a90ffdca7347b2ece1354d6e8e96632b26af0316
graceanglc <graceanglc@gmail.com>
2017-05-13 20:28:09 +0700

    HasilSeleksi & KartuUjian udah bener

--- hasilseleksi.php
+++ hasilseleksi.php
@@ -20,7 +20,7 @@
 
     function lihatHasilSeleksiLulus($idPendaftaran) {
         $connect = connectDB();
-        $command = "SELECT PD.id, P.nama_lengkap, PP.status_lulus, PS.nama, PD.npm FROM SIRIMA.PENDAFTARAN AS SIRIMA.PD, SIRIMA.PELAMAR AS P, SIRIMA.PENDAFTARAN_PRODI AS PP, SIRIMA.PROGRAM_STUDI AS PS WHERE PP.id_pendaftaran = PD.id AND PD.pelamar = P.username AND PD.id = '$idPendaftaran' AND PS.kode = PP.kode_prodi";
+        $command = "SELECT PD.id, P.nama_lengkap, PP.status_lulus, PS.nama, PD.npm, PS.jenjang, PS.jenis_kelas FROM SIRIMA.PENDAFTARAN AS PD, SIRIMA.PELAMAR AS P, SIRIMA.PENDAFTARAN_PRODI AS PP, SIRIMA.PROGRAM_STUDI AS PS WHERE PP.id_pendaftaran = PD.id AND PD.pelamar = P.username AND PD.id = '$idPendaftaran' AND PS.kode = PP.kode_prodi";
         $result =  pg_query($connect, $command);
         if(!$result) {
             die("Error ouccured while getting query :").pg_last_error();
@@ -32,15 +32,8 @@
         if($_POST['command'] === 'Lihat'){
             $_SESSION['id-pendaftaran'] = $_POST['pendaftaran'];
             $result = pg_fetch_row(cekHasilSeleksi($_SESSION['id-pendaftaran']));
-            $status = $result[2];
-
-            echo json_encode($result);
-            $flag = false;
-            if($status === "t") {
+            if($result[2] === "t") {
                 $result = pg_fetch_row(lihatHasilSeleksiLulus($_SESSION['id-pendaftaran']));
-                $flag = true;
-            }else {
-                $result = pg_fetch_row(cekHasilSeleksi($_SESSION['id-pendaftaran']));
             }
             echo json_encode($result);
         }
